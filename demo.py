3af7b2bbced150a81db5952c438a1446a5810815
jesusperiago <jesusperiago@gmail.com>
2016-03-20 13:53:23 +0000

    Update demo

--- demo.py
+++ demo.py
@@ -1,8 +1,13 @@
 from fonAPI import FonApi
 
-fon = FonApi()
+fon = FonApi('24875e2d23e5583dab960129306139fb60db0c3b6f01144c')
 
-device = 'lenovo'
-
-for phone in fon.getdevice(device):
-    print phone['DeviceName']
+device = '3210'
+phones = fon.getdevice(device)
+try:
+    for phone in phones:
+        print phone['DeviceName']
+        print phone['weight']
+        print phone['resolution']
+except:
+    print phones
