58fff1948925f813c3bbe18b95db7a72587e736a
Jake Sherman <jake@jakesherman.com>
2018-05-30 10:12:54 -0400

    Fixing an "ImportError: No module named req" error where pip.req was not found

--- setup.py
+++ setup.py
@@ -14,9 +14,13 @@ except:
         long_description = f.read()
 
 
-from pip.req import parse_requirements
-install_requires = [str(ir.req) for ir in parse_requirements(
-    path.join(here, 'requirements.txt'), session=False)]
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+install_requires = parse_requirements('requirements.txt')
 
 
 __title__ = 'fonoapi'
